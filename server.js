df59a285e263f853a44ff6861760cac28b41468b
Ryan Talbert <ryantalbert@Ryans-Macintosh.local>
2020-07-27 11:26:57 -0500

    add read.me

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room });
+  res.render('room', { roomId: req.params.room, portId: port });
 });
 
 io.on('connection', (socket) => {
